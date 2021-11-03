b5b38505ba3ddae42ba9489bdd86cd2327e1e2a6
Kruril <guillaume.charlier.2017@gmail.com>
2021-11-03 10:40:41 +0100

    Frontend: initilize base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Error from "./routes/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router basename={process.env.REACT_APP_ROUTER_BASE || ""}>
+        <div className="flex flex-col h-screen">
+          <div className="flex flex-1 flex-col">
+            <Switch>
+              <Route exact component={Error} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
   );
 }
 
